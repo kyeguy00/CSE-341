2a8e4524c59d3cefee97116888d16ebb86489b90
kyeguy00 <91562786+kyeguy00@users.noreply.github.com>
2023-02-13 18:48:34 -0700

    Commented out developer testing

--- connect.test.js
+++ connect.test.js
@@ -1,48 +1,33 @@
-// const app = require('./test-server.js');
-const request = require('supertest');
-const {app, startApp} = require('./app.js');
+// const request = require("supertest");
+// const { app, startApp } = require("./app.js");
 
-beforeAll(async () => {await startApp();});
-describe('GET /players', () => {
-    
+// beforeAll(async () => {
+//   await startApp();
+// });
+// describe("GET /players", () => {
+//   it("should return an array of players", async () => {
+//     const res = await request(app).get("/players");
 
-    it('should return an array of players', async () => {
-    const res = await request(app).get('/players');
-    // console.log(res)
-    expect(res.statusCode).toBe(200);
-    // done();
-    });
-});
+//     expect(res.statusCode).toBe(200);
+//   });
+// });
 
+// describe("GET /teams", () => {
+//   it("should return an array of teams", async () => {
+//     const res = await request(app).get("/teams");
+//     expect(res.statusCode).toBe(200);
+//   });
+// });
 
-describe('GET /teams', ()=> {
-    // beforeAll(async () => {
-    //     await startApp();
-    // });
-
-    it('should return an array of teams', async () => {
-        const res = await request(app).get('/teams');
-        expect(res.statusCode).toBe(200);
-    });
-});
-
-describe('GET /arenas', ()=> {
-    // beforeAll(async () => {
-    //     await startApp();
-    // });
-
-    it('should return an array of teams', async () => {
-        const res = await request(app).get('/arenas');
-        expect(res.statusCode).toBe(200);
-    });
-});
-describe('GET /positions', ()=> {
-    // beforeAll(async () => {
-    //     await startApp();
-    // });
-
-    it('should return an array of teams', async () => {
-        const res = await request(app).get('/positions');
-        expect(res.statusCode).toBe(200);
-    });
-});
+// describe("GET /arenas", () => {
+//   it("should return an array of teams", async () => {
+//     const res = await request(app).get("/arenas");
+//     expect(res.statusCode).toBe(200);
+//   });
+// });
+// describe("GET /positions", () => {
+//   it("should return an array of teams", async () => {
+//     const res = await request(app).get("/positions");
+//     expect(res.statusCode).toBe(200);
+//   });
+// });
